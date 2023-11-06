fc5aac843c34a38a50eec4c09f514388fb987c69
Elena Bronina <aplicaziya@yandex.ru>
2023-11-06 12:22:45 +0300

    add logic

---
+++ brain_games/games/logic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import prompt
+
+
+def welcome():
+    name_user = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
+    print(f'Hello, {name_user}!')
+    return name_user
+
+
+def answer():
+    answer = prompt.string('Your answer: ' )
+    return answer
