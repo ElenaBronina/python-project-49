db0911988997270dc3f80189a7d794d99686229d
Elena Bronina <aplicaziya@yandex.ru>
2023-11-19 15:36:33 +0300

    edit brain_calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,41 +3,16 @@ from brain_games.logic import answer
 from brain_games.logic import welcome
 
 
-def sum():
-    a = random.choice(range(0, 100))
-    b = random.choice(range(0, 100))
-    return ((a + b), str(a) + "+" + str(b))
-
-
-def diff():
-    c = random.choice(range(50, 100))
-    d = random.choice(range(0, 50))
-    return ((c - d), str(c) + "-" + str(d))
-
-
-def mult():
-    e = random.choice(range(0, 10))
-    f = random.choice(range(0, 10))
-    return ((e * f), str(e) + "*" + str(f))
-
-
-def random_choice():
-    x = sum()
-    y = diff()
-    z = mult()
-    list = [x, y, z]
-    return random.choice(list)
-
-
 def brain_calc():
     name_user = welcome()
     print('What is the result of the expression?')
     i = 1
     while i <= 3:
-        random_question = random_choice()
-        question_user = random_question[1]
-        right_answer = random_question[0]
-        print(f'Question: {question_user}')
+        num_1 = random.choice(range(0, 30))
+        num_2 = random.choice(range(0, 30))
+        oper = random.choice(['+', '*', '-'])
+        right_answer = int(eval(f'{num_1} {oper} {num_2}'))
+        print(f'Question: {num_1} {oper} {num_2}')
         answer_user = answer()
         if answer_user != str(right_answer):
             return print(f"{answer_user} is wrong answer ;(. \
