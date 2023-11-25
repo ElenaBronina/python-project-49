cbcef0a2029ef9ae998ed11180e3694e1c7973bf
Elena Bronina <aplicaziya@yandex.ru>
2023-11-25 18:26:11 +0300

    edit2 games_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,19 +4,18 @@ from brain_games.logic import answer
 from brain_games.logic import welcome
 
 
-def is_prime():
-    random_number = random.randint(2, 25)
+def is_prime(random_number):
     if random_number < 2:
-        return False
+        return 'no'
     if random_number == 2:
-        return True
+        return 'yes'
     limit = math.sqrt(random_number)
     i = 2
     while i <= limit:
         if random_number % i == 0:
-            return random_number, False
+            return 'no'
         i += 1
-    return random_number, True
+    return 'yes'
 
 
 def brain_prime():
@@ -24,19 +23,14 @@ def brain_prime():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     i = 1
     while i <= 3:
-        prime_or_not = is_prime()
-        right_answer = 'yes' if prime_or_not[1] else 'no'
-        print(f'Question: {prime_or_not[0]}')
+        random_number = random.randint(2, 30)
+        print(f'Question: {random_number}')
         answer_user = answer()
-        if answer_user == 'yes' and prime_or_not[1]:
+        right_answer = is_prime(random_number)
+        if answer_user == right_answer:
             print('Correct!')
-            i = i + 1
-        if answer_user == 'no' and not prime_or_not[1]:
-            print('Correct!')
-            i = i + 1
-        elif (answer_user != 'yes'
-                and prime_or_not[1] or answer_user != 'no'
-                and not prime_or_not[1]):
+        i = i + 1
+        if answer_user != right_answer:
             return print(f"{answer_user} is wrong answer ;(. \
 Correct answer was {right_answer}. \
 \nLet's try again, {name_user}!")
